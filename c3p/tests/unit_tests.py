d1e9b15a67394b07da8c3cfe392d1c28e9125cab
Jeff <jeff.smits@gmail.com>
2012-04-04 17:22:36 +0200

    tests update. I added a unit test class for the Main class. You can use C3P_test.py to run all the tests, and change the line of commented code to chose just acceptance tests or just unit tests. I also fixed a previously unknown bug in Main. Take a look at the diff if you're interested, but I don't think it's worthy of describing fully. I'm not closing issue #9 yet, the namespaces file still needs a unit testing.
    Signed-off-by: Jeff <jeff.smits@gmail.com>

---
+++ c3p/tests/unit_tests.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Python 3.2 code
+#
+# Copyright (c) 2012 Jeff Smits
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# C3P - C-compatible code preprocessor
+# This commandline tool reads a file and expands macro's. 
+#
+# This file is a utility file and doesn't contain the whole tool. 
+# Also it does not run standalone. 
+#
+# This file just imports all the unit tests
+
+from .main_unit_test import Main_unit_test
